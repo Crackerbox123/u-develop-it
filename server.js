7b1119a7d8be5cdd9d850801b95633dc18a93eaa
Kal O <koestreicher4@gmail.com>
2022-08-08 12:15:19 -0500

    query statements + primary key bug fix

--- server.js
+++ server.js
@@ -21,8 +21,42 @@ const db = mysql.createConnection(
         database: 'election'
     },
     console.log('Connected to the election database.')
-)
+);
 
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows)
+// });]
+
+// GET a single candidate
+//db.query(`SELECT * FROM candidates WHERE id = 20`, (err, row) => {
+//    if (err) {
+//        console.log(err);
+//    }
+//    console.log(row);
+//});
+
+// Delete a candidate
+//db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//    if (err) {
+//        console.log(err);
+//    }
+//
+//    console.log(result);
+//})
+//
+// Create a candidate
+// Create a candidate
+//const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+//              VALUES (?,?,?,?)`;
+//const params = [1, 'Ronald', 'Firbank', 1];
+//
+//db.query(sql, params, (err, result) => {
+//  if (err) {
+//    console.log(err);
+//  }
+//  console.log(result);
+//});
+// 
 
 // DEFAULT response for any other request (NOt Found)
 app.use((req, res) => {
